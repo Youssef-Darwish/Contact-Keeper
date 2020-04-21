ce63e7bb2dd6eb06c21d117ee8ebd4b0e6e7697e
Youssef Darwish <youssefdarwish95@gmail.com>
2020-04-21 14:19:47 +0200

    Register User and Config Folder

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const users = require('./routes/users');
 const contacts = require('./routes/contacts');
 const auth = require('./routes/auth');
-
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
     res.json({ msg: 'Welcome to the contact keepr api' })
