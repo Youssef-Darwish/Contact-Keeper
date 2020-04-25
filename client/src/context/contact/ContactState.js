6c01ff5cd05f4382f50c1b56e1563557b59bc049
Youssef Darwish <youssefdarwish95@gmail.com>
2020-04-25 10:30:21 +0200

    Filter Contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -40,7 +40,8 @@ const ContactState = props => {
             },
 
         ],
-        current: null
+        current: null,
+        filtered: null
     };
 
 
@@ -71,12 +72,21 @@ const ContactState = props => {
 
 
     //Update Contact
+    const updateContact = (contact) => {
+        dispatch({ type: UPDATE_CONTACT, payload: contact });
+    };
 
 
     //Filter Contacts
+    const filterContacts = (text) => {
+        dispatch({ type: FILTER_CONTACTS, payload: text });
+    }
 
 
     //Clear Filter
+    const clearFilter = () => {
+        dispatch({ type: CLEAR_FILTER });
+    }
 
 
 
@@ -87,10 +97,14 @@ const ContactState = props => {
             value={{
                 contacts: state.contacts,
                 current: state.current,
+                filtered: state.filtered,
                 addContact,
                 deleteContact,
                 setCurrent,
-                clearCurrent
+                clearCurrent,
+                updateContact,
+                filterContacts,
+                clearFilter
             }}
         >
             {props.children}
