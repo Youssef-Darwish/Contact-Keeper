09e9c554c5bb04de35004d8bdbf4603c20483595
Youssef Darwish <youssefdarwish95@gmail.com>
2020-05-02 22:41:34 +0200

    Login and Private Route

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -96,7 +96,9 @@ const AuthState = props => {
 
     //Logout
     const logout = () => {
-        console.log("load");
+        dispatch({
+            type: LOGOUT
+        });
     }
 
     //Clear Errors
